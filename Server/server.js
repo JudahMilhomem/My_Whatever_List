5a82602788295e6480085d107748ec22add2e8ae
JudahMilhomem <judahmilhomem2001@gmail.com>
2024-08-21 15:44:15 -0300

    Express routes
    Basic CRUD routes structure.

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,60 @@
 require('dotenv').config();
 const express = require('express'); // importa o express
-
 const app = express(); // cria uma instncia do express
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { // inicia o servidor na porta 3000
     console.log(`Server up and running on port ${port}`);
+});
+
+// ----- ROUTES ---- //
+// get all games
+app.get('/api/v1/games', (req, res) => {
+    res.status(200).json({
+        status: 'Success!',
+        data: {
+            game_01: ['Resident Evil 4 Remake', '2023']
+        }
+    });
+});
+
+// get a game
+app.get('/api/v1/games/:id', (req, res) => { // ':id' -> url parameter
+    // console.log(req.params);
+    res.status(200).json({
+        status: 'Success!',
+        data: {
+            game_02: ['Devil May Cry V', '2019']
+        }
+    });
+});
+
+// create a game
+app.post('/api/v1/games/:id', (req, res) => {
+    // console.log(req.params.id);
+    // console.log(req.body);
+    res.status(201).json({
+        status: 'Success!',
+        data: {
+            game_03: ['Broforce', '2013']
+        }
+    });
+});
+
+// update a game
+app.put('/api/v1/games/:id', (req, res) => {
+    res.status(200).json({
+        status: 'Success!',
+        data: {
+            game_03: ['Broforce', '2014']
+        }
+    }); 
+});
+
+// delete a game
+app.delete('/api/v1/games/:id', (req, res) => {
+    res.status(204).json({ // no content
+        status: 'Deleted!'
+    }); 
 });
--- db.sql
+++ db.sql
@@ -1,29 +1,29 @@
-CREATE TABLE movies(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    movie_name VARCHAR(50) NOT NULL,
-    rdate DATE NOT NULL, -- release date
-    synopsis TEXT(300),
-    director VARCHAR(30) NOT NULL,
-    srating DOUBLE PRECISION check(srating >= 1 AND srating <= 5) -- "star" rating (01 to 5)
-);
+-- CREATE TABLE movies(
+--     id BIGSERIAL NOT NULL PRIMARY KEY,
+--     movie_name VARCHAR(50) NOT NULL,
+--     rdate DATE NOT NULL, -- release date
+--     synopsis TEXT,
+--     director VARCHAR(30) NOT NULL,
+--     srating DOUBLE PRECISION check(srating >= 1 AND srating <= 5) -- "star" rating (01 to 5)
+-- );
 
-CREATE TABLE animes(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    anime_name VARCHAR(50) NOT NULL,
-    rdate DATE NOT NULL,
-    synopsis TEXT(300),
-    author VARCHAR(30) NOT NULL,
-    -- studio
-    srating DOUBLE PRECISION check(srating >= 1 AND srating <= 5),
-    finished BOOLEAN NOT NULL
-);
+-- CREATE TABLE animes(
+--     id BIGSERIAL NOT NULL PRIMARY KEY,
+--     anime_name VARCHAR(50) NOT NULL,
+--     rdate DATE NOT NULL,
+--     synopsis TEXT,
+--     author VARCHAR(30) NOT NULL,
+--     -- studio
+--     srating DOUBLE PRECISION check(srating >= 1 AND srating <= 5),
+--     finished BOOLEAN NOT NULL
+-- );
 
 CREATE TABLE games(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     game_name VARCHAR(50) NOT NULL,
     rdate DATE NOT NULL,
-    descr TEXT(300), -- description
+    descr TEXT, -- description
     -- tags (categories. Ex.: action, horror)
     company VARCHAR(20) NOT NULL,
-    dmc_rating VARCHAR(3),
-)
+    dmc_rating VARCHAR(3)
+);
