3cfdececa2ca9b81eddf1cdf616580c42803bbcf
JudahMilhomem <judahmilhomem2001@gmail.com>
2024-09-02 11:52:22 -0300

    Form fields useState
    Added an use state for each form input field, to keep track of what is typed.
    * Also added some developer notes.

--- client/src/components/AddGameForm/AddGameForm.jsx
+++ client/src/components/AddGameForm/AddGameForm.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddGameForm.css'
 
 const AddGameForm = () => {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [company, setCompany] = useState("");
+  const [description, setDescription] = useState("");
+
+
   return (
     <div className="flex-container form-section-box">
         <section className="form-section">
@@ -10,22 +16,52 @@ const AddGameForm = () => {
 
                 <div className="flex-container input-field-box">
                     <label htmlFor="game-name">Name <span>*</span></label>
-                    <input type="text" className="input-field name-input" id="game-name" placeholder="Insert a name" required/>
+                    <input
+                        type="text"
+                        className="input-field name-input"
+                        id="game-name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} // ??
+                        placeholder="Insert a name"
+                        required
+                    />
                 </div>
 
                 <div className="flex-container input-field-box">
                     <label htmlFor="release-date">Release <span>*</span></label>
-                    <input type="text" className="input-field release-input" id="release-date" placeholder="Insert the release date" required/>
+                    <input
+                        type="text"
+                        className="input-field release-input"
+                        id="release-date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        placeholder="Insert the release date"
+                        required
+                    />
                 </div>
 
                 <div className="flex-container input-field-box">
                     <label htmlFor="company-name">Company <span>*</span></label>
-                    <input type="text" className="input-field company-input" id="company-name" placeholder="Insert the game's Company" required/>
+                    <input
+                        type="text"
+                        className="input-field company-input"
+                        id="company-name"
+                        value={company}
+                        onChange={(e) => setCompany(e.target.value)}
+                        placeholder="Insert the game's company"
+                        required
+                    />
                 </div>
 
                 <div className="flex-container textarea-field-box">
                     <label htmlFor="game-description">Description</label>
-                    <textarea className="textarea description" id="game-description" placeholder="Add a description (optional)"></textarea>
+                    <textarea
+                        className="textarea description"
+                        name="game-description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Add a description (optional)"
+                    ></textarea>
                 </div>
 
                 <button className="submit-button type-1-button">Add new game</button>
---
+++ developer_notes.md
@@ -0,0 +1,8 @@
+# Features/Implement:
+- Responsive layout;
+- Navigation bar;
+- (...)
+
+# Fix:
+Nothing to see here... <br>
+![This is fine](https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXh3MjBwYWNpOG1rZHMwMHBtMzRjZzkzbzM5YXltenZvdjdid3dkdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NTur7XlVDUdqM/giphy.webp)
